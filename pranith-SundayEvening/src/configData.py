9f68e83f72e7f363e1498e4d7950ab95a8a6d5e5
ThePlom <98788495+ThePlom@users.noreply.github.com>
2022-09-11 17:38:22 -0400

    Create configData.py

---
+++ pranith-SundayEvening/src/configData.py
@@ -0,0 +1,33 @@
+
+
+CONFIG_DATA = {
+    'Regular-1': {
+            'boardSize': [5, 5],
+            'candyFreq': {
+                'candy': 2,
+                'candy2': 1,
+            },
+            'completionCriteria': {
+                    'candy': 4,
+                    'candy2': 7,
+            },
+            'turns': 20,
+    },
+    'Fruit-1': {
+                'boardSize': [5, 5],
+                'candyFreq': {
+                    'red': 1,
+                    'yellow': 1,
+                    'green': 3,
+                    'blue': 5
+                },
+                'completionCriteria': {
+                    'red': 0,
+                    'yellow': 0,
+                    'green': 0,
+                    'blue': 0,
+                    'fruit': 2,
+                },
+                'turns': 20,
+        }
+}
