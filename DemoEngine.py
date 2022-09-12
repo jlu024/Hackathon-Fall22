dfad18a3eed6de045c2fbab0b308c4398fc263db
jlu024 <103219124+jlu024@users.noreply.github.com>
2022-09-11 20:25:31 -0400

    Update DemoEngine.py
    included checkBoard that follows' candy crush compare +3 candies, crush them, and gravity motion + randomize the empty spaces.

--- DemoEngine.py
+++ DemoEngine.py
@@ -1,26 +1,69 @@
-
+import random
 class GameState():
     def __init__(self):
         self.board = [
             ["--", "--", "--", "--", "--", "--", "--", "--"],
             ["--", "--", "--", "--", "--", "--", "--", "--"],
             ["--", "--", "--", "--", "--", "--", "--", "--"],
-            ["--", "--", "candy", "candy2", "candy", "candy", "--", "--"],
+            ["candy", "candy2", "candy3", "candy4", "candy5", "candy6", "candy", "--"],
             ["--", "--", "--", "--", "--", "--", "--", "--"],
             ["--", "--", "--", "--", "--", "--", "--", "--"],
             ["--", "--", "--", "--", "--", "--", "--", "--"],
             ["--", "--", "--", "--", "--", "--", "--", "--"]]
-            # ["candy", "candy2", "candy", "candy2", "candy", "candy", "candy2", "candy"],
-            # ["candy", "candy2", "candy", "candy2", "candy", "candy", "candy2", "candy"],
-            # ["candy", "candy2", "candy", "candy2", "candy", "candy", "candy2", "candy"],
-            # ["candy", "candy2", "candy", "candy2", "candy", "candy", "candy2", "candy"],
-            # ["candy", "candy2", "candy", "candy2", "candy", "candy", "candy2", "candy"],
-            # ["candy", "candy2", "candy", "candy2", "candy", "candy", "candy2", "candy"],
-            # ["candy", "candy2", "candy", "candy2", "candy", "candy", "candy2", "candy"],
-            # ["candy", "candy2", "candy", "candy2", "candy", "candy", "candy2", "candy"]]
 
         #self.whiteToMove = True
         #self.moveLog = []
+
+    def randomizeBoard(self):
+        candies = ["candy", "candy2", "candy3", "candy4", "candy5", "candy6"]
+        for x in range(len(self.board)):
+            for y in range(len(self.board[x])):
+                if self.board[x][y] == "--":
+                    self.board[x][y] = random.choice(candies)
+        return self.board
+
+    #print(randomizeBoard(board))
+
+    def checkBoard(self):
+        while True:
+        #break when comparison failed in not crush
+            #RANDOMIZE EMPTY SPACES when a crush is successful
+            candies = ["candy", "candy2", "candy3", "candy4", "candy5", "candy6"]
+            for x in range(len(self.board)):
+                for y in range(len(self.board[x])):
+                    if self.board[x][y] == "--":
+                        self.board[x][y] = random.choice(candies)
+            #COMPARE motion
+            crush = set()
+            #go through different rows
+            for i in range(len(self.board)):
+                #across the column
+                for j in range(len(self.board[0])):
+                    #added to crush when 3+ candies same
+                    if i>1 and self.board[i][j] != "--" and self.board[i][j]==self.board[i-1][j]==self.board[i-2][j]:
+                        #checks in three at a time, re-defines crush with unique values
+                        crush |= {(i, j), (i-1, j), (i-2, j)}
+                    if j > 1 and self.board[i][j] != "--" and self.board[i][j] == self.board[i][j-1] == self.board[i][j-2]:
+                        crush |= {(i, j), (i, j-1), (i, j-2)}
+            #CRUSH the candies
+            if not crush:
+                break
+            #crushed candies exist
+            for i,j in crush:
+                self.board[i][j] = "--"
+
+            #DROP, first going through the rows
+            for j in range(len(self.board[0])):
+                idx = len(self.board)-1
+                #moving across the vertical
+                for i in reversed(range(len(self.board))):
+                    #if this cell is candy, then change to new candy image and reduce idx by 1
+                    if self.board[i][j] != "--":
+                        self.board[idx][j]=self.board[i][j]
+                        idx -= 1
+                for i in range(idx+1):
+                    self.board[i][j] = "--"
+        return self.board
 
     def makeMove(self, move):
 
@@ -30,6 +73,8 @@ class GameState():
             #swapped location of start candy coord with end, and end candy coord with start
             self.board[move.startRow][move.startColumn] = move.pieceCaptured
             self.board[move.endRow][move.endColumn] = move.pieceMoved
+
+        #not included log and player turns
 
     # def getValidMoves(self):
     #     return self.getAllPossibleMoves
@@ -56,3 +101,4 @@ class Move():
 
         self.pieceMoved = board[self.startRow][self.startColumn]
         self.pieceCaptured = board[self.endRow][self.endColumn]
+
